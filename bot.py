9b6df65774558ca886eba295f43d5e7e7a204cd0
Stas Mureveynik <a2kat_90@mail.ru>
2024-10-09 20:12:35 +0300

    add unauthorized users and non-text message filter

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ ADMIN_ID: str = utils.load_confidentials_from_env("ADMIN_ID")
 class BetBot(telebot.TeleBot):
 
     def __init__(self):
-        super().__init__(token=TELEGRAM_TOKEN, parse_mode=None)
-        self.register_message_handler(callback=self.handle_message, func=self.filter_users)
+        super().__init__(token=TELEGRAM_TOKEN, parse_mode='HTML')
+        self.register_message_handler(callback=self.handle_message, func=self.message_filter)
 
     def start(self) -> None:
         self.notify_admin("<b>БОТ ЗАПУЩЕН</b>")
@@ -28,26 +28,37 @@ class BetBot(telebot.TeleBot):
         self.send_message(chat_id=ADMIN_ID, text=prefix + text, parse_mode='HTML')
 
     def handle_message(self, message: telebot.types.Message) -> None:
+        """Handles all text messages recieved by auth users."""
         text = f"A message has been received\n" \
-                 f"Sender: {message.from_user.first_name} {message.from_user.last_name}\n" \
-                 f"Text: {message.text}"
+               f"Sender: {message.from_user.first_name} {message.from_user.last_name}\n" \
+               f"Text: {message.text}"
         self.notify_admin(text=text)
 
-    def filter_users(self, message: telebot.types.Message) -> bool:
+    def message_filter(self, message: telebot.types.Message) -> bool:
         """
-        Checks if the user is authorized and sends a denial message if not. Used as a filter for message handling.
+        Filter for all messages coming from users. Declines unauthorized users and all non-text messages.
+        Sends replies describing the situation to message senders.
 
         :param message: The incoming message object from a user.
-        :return: True if user is authorized, False otherwise.
+        :return: True if a message is allowed to be processed by bot, False otherwise.
         """
-        user_authorized = message.from_user.id == int(ADMIN_ID)
-        if not user_authorized:
-            user_id = message.from_user.id
-            self.send_message(chat_id=user_id,
-                              parse_mode='HTML',
-                              text='<b>Доступ запрещен!</b>\n\n'
-                                   'К сожалению, это закрытое соревнование, и Вы не являетесь его участником. 😢')
-        return user_authorized
+
+        message_is_text = message.content_type == 'text'
+
+        if not self.user_authorized(message.from_user.id):
+            self.reply_to(message=message,
+                          text=f"<b>Доступ запрещен!</b>\n\n"
+                               f"К сожалению, это закрытое соревнование, и Вы не являетесь его участником. 😢"
+                          )
+            return False
+        if not message_is_text:
+            self.reply_to(message=message, text=f"<b>Ой!</b>\nЭтот бот понимает только текстовые сообщения.")
+            return False
+        return True
+
+    @staticmethod
+    def user_authorized(user_id: int) -> bool:
+        return user_id == int(ADMIN_ID)
 
 
 if __name__ == "__main__":
