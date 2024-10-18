9035cde109bf20a43996905cbce853dd821405fa
Stas Muraveynik <a2kat_90@mail.ru>
2024-10-18 20:57:18 +0300

    refactor: User class now has proper bool field values while reading from MySQL db instead of 0s and 1s

--- users.py
+++ users.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, fields
 from datetime import datetime
 
 
@@ -7,15 +7,24 @@ class User:
     telegram_id: int
     created_at: datetime  # default value is set in a create table query
     id: int = None
-    chat_id: int = None
+    used_bot: bool = False
+    blocked_bot: bool = False
     is_admin: bool = False
     first_name: str = None
     last_name: str = None
     last_updated: datetime = None
 
     def to_dict(self) -> dict:
-        return asdict(self)
+        # a dict of class_field: field_value pairs
+        fields_dict = {f.name: self.__getattribute__(f.name) for f in fields(self)}
+        # as MySQL stores bool values as 0s and 1s we manually transform bool fields
+        bool_fields = self._get_bool_fields()
+        return {k: (v if k not in bool_fields else bool(v)) for k, v in fields_dict.items()}
 
     @classmethod
-    def from_dict(cls, d):
+    def from_dict(cls, d: dict) -> 'User':
         return User(**d)
+
+    @classmethod
+    def _get_bool_fields(cls) -> tuple[str]:
+        return tuple(f.name for f in fields(cls) if f.type == bool)
